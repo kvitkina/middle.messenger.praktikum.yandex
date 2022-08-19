1c01752d654d570aa67f6e8334e3e234c95a4b50
kvitkina <kvitkina@advantum.pro>
2022-08-19 18:24:55 +0300

    add profile inputs

---
+++ src/pages/Profile/ProfileInput/index.js
@@ -0,0 +1,6 @@
+import tmpl from './ProfileInput.hbs';
+import './ProfileInput.scss';
+
+export default(input) => {
+    return tmpl({input});
+};
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,20 +1,69 @@
 import tmpl from './Profile.hbs';
 import ProfileButton from '../../components/ProfileButton';
-
+import ProfileInput from './ProfileInput';
+import Button from '../../components/Button';
 import './Profile.scss';
 
 const User = {
-    mail: 'pochta@yandex.ru',
-    login: 'ivanivanov',
-    first_name: 'Иван',
-    second_name: 'Иванов',
+    email: 'pochta@yandex.ru',
+    login: 'kvitkina',
+    first_name: 'Ирина',
+    second_name: 'Квиткина',
     phone: '+7 (909) 967 30 30',
-    display_name: 'Иван'
-}
+    display_name: 'kvitkina',
+};
+
+const profileInputs = [
+    {
+        label: 'Почта',
+        type: 'email',
+        value: `${User.email}`,
+        name: 'email',
+        disabled: false,
+    },
+    {
+        label: 'Логин',
+        type: 'text',
+        value: `${User.login}`,
+        name: 'login',
+        disabled: false,
+    },
+    {
+        label: 'Имя',
+        type: 'text',
+        value: `${User.first_name}`,
+        name: 'first_name',
+        disabled: true,
+    },
+    {
+        label: 'Фамилия',
+        type: 'text',
+        value: `${User.second_name}`,
+        name: 'second_name',
+        disabled: false,
+    },
+    {
+        label: 'Имя в чате',
+        type: 'text',
+        value: `${User.display_name}`,
+        name: 'display_name',
+        disabled: false,
+    },
+    {
+        label: 'Телефон',
+        type: 'phone',
+        value: `${User.phone}`,
+        name: 'phone',
+        disabled: false,
+    },
+
+];
 
 const Profile = tmpl({
 	user: User,
-    actions: [ProfileButton('Поменять данные'), ProfileButton('Поменять пароль')]
+    inputs: profileInputs.map((item) => ProfileInput(item)),
+    actions: [ProfileButton('Изменить данные'), ProfileButton('Изменить пароль')],
+    saveButton: Button('Сохранить'),
 })
 
-export default Profile
+export default Profile;
