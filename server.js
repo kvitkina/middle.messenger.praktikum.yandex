b9c18835c598ceefd30c863b4ed1fed935ebdd03
Irina Kvitkina <irina.kvitkina@gmail.com>
2022-11-14 00:06:43 +0300

    fix server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const history = require('express-history-api-fallback');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // eslint-disable-next-line no-undef
 app.use(express.static(`${__dirname}/dist`));
+app.use(history('dist/index.html', { root: __dirname }));
 
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}!`);
